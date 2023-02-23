d78996ab158935971e7599c19e2f82a090da431b
jpared3s <jparedes.wall@gmail.com>
2023-02-22 21:31:32 -0500

    status component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointments/Header";
 import Empty from "components/Appointments/Empty";
 import Show from "components/Appointments/Show";
 import Confirm from "components/Appointments/Confirm";
+import Status from "components/Appointments/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -158,4 +159,5 @@ storiesOf("Appointment", module)
     message="Delete the appointment?"
     onConfirm={action("onConfirm")}
     onCancel={action("onCancel")}
-  />);
+  />)
+  .add("Status", () => <Status message="Deleting"/>);
