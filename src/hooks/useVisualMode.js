433f6a0a8544613fc5c6d39d1b03937bcdb0b23a
jpared3s <jparedes.wall@gmail.com>
2023-02-25 17:23:45 -0500

    custom hooks assignment

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    setHistory(prevHistory => {
+      if (replace) {
+        const newHistory = [...prevHistory.slice(0, -1), newMode];
+        return newHistory;
+      } else {
+        const newHistory = [...prevHistory, newMode];
+        return newHistory;
+      }
+    });
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = [...history.slice(0, -1)];
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
